1b42d3cb572d3a094418b1bb2e2d7c4168d3422e
Harshit Rajgarhia <hrajgarhia943@gmail.com>
2019-01-02 18:57:53 +0530

    Solving linear regression using:-
    1. Normal equation
    2. Batch Gradient Descent
    3. Gradient calculation using autodiff
    4. GradientDescentOptimizer
    5. MomentumOptimizer

--- tensorflow.py
+++ tensorflow.py
@@ -19,6 +19,8 @@ sess.close()
 
 # Writing sess.run() multiple times can be cumbersome. To avoid that we can use "with" which will automatically close
 # the session when it runs and evaluates the results
+# inside the with block, the session is set as the default session. Calling x.initializer.run() is equivalent to
+# calling tf.get_default_session().run(x.initializer)
 
 with tf.Session() as sess:
     x.initializer.run()
@@ -34,6 +36,7 @@ init=tf.global_variables_initializer()
 
 with tf.Session() as sess:
     init.run()
+    result = f.eval()
 
 
 #Inside Jupyter or within a Python shell you may prefer to create an InteractiveSession. The only
@@ -92,14 +95,36 @@ with tf.Session() as sess:
     theta_value = theta.eval()
 
 
+# compare the above calculation of theta by doing it with pure numpy
+
+X=housing_data_plus_bias
+y=housing.target.reshape(-1,1)
+theta_numpy = np.linalg.inv(X.T.dot(X)).dot(X.T).dot(y)
+
+# compare the above calculation of theta by doing it with sklearn
+
+from sklearn.linear_model import LinearRegression
+lin_reg= LinearRegression()
+lin_reg.fit(housing.data,housing.target.reshape(-1,1))
+print(np.r_[lin_reg.intercept_.reshape(-1,1),lin_reg.coef_.T])
+
+
 # Linear regression gradient descent solution using tensorflow
+
+
+# Gradient descent requires scaling the feature vectors first.
+
+from sklearn.preprocessing import StandardScaler
+scalar =  StandardScaler()
+scaled_housing_data =  scalar.fit_transform(housing.data)
+scaled_housing_data_plus_bias= np.c_[np.ones((m,1)),scaled_housing_data]
 
 n_epochs = 1000
 learning_rate = 0.01
 
-X = tf.constant(housing_data_plus_bias, dtype=tf.float32, name="X")
+X = tf.constant(scaled_housing_data_plus_bias, dtype=tf.float32, name="X")
 y = tf.constant(housing.target.reshape(-1, 1), dtype=tf.float32, name="y")
-theta = tf.Variable(tf.random_uniform([n+1,1],-1.0,1.0),name='theta')
+theta = tf.Variable(tf.random_uniform([n+1,1],-1.0,1.0,seed =42),name='theta')
 y_pred = tf.matmul(X,theta,name='predictions')
 error = y_pred-y
 mse = tf.reduce_mean(tf.square(error), name="mse")
@@ -115,3 +140,136 @@ with tf.Session() as sess:
         sess.run(training_op)
     best_theta = theta.eval()
 
+# Gradient descent using autodiff
+
+def reset_graph(seed=42):
+    tf.reset_default_graph()
+    tf.set_random_seed(seed)
+    np.random.seed(seed)
+
+reset_graph()
+
+n_epochs = 1000
+learning_rate = 0.01
+
+X = tf.constant(scaled_housing_data_plus_bias, dtype=tf.float32, name="X")
+y = tf.constant(housing.target.reshape(-1, 1), dtype=tf.float32, name="y")
+theta = tf.Variable(tf.random_uniform([n + 1, 1], -1.0, 1.0, seed=42), name="theta")
+y_pred = tf.matmul(X, theta, name="predictions")
+error = y_pred - y
+mse = tf.reduce_mean(tf.square(error), name="mse")
+gradients =  tf.gradients(mse,[theta])[0]
+training_op = tf.assign(theta, theta - learning_rate * gradients)
+
+init = tf.global_variables_initializer()
+
+with tf.Session() as sess:
+    sess.run(init)
+
+    for epoch in range(n_epochs):
+        if epoch % 100 == 0:
+            print("Epoch", epoch, "MSE =", mse.eval())
+        sess.run(training_op)
+
+    best_theta = theta.eval()
+
+print("Best theta:")
+print(best_theta)
+
+
+# practicing autodiff
+
+def my_func(a,b):
+    z=0
+    for i in range(100):
+        z = a * np.cos(z + i) + z * np.sin(b - i)
+    return z
+
+my_func(0.2,0.3)
+
+# let's compute the function at a = 0.2 and b = 0.3 and the partial derivative
+# of that point with regards to a and b respectively
+
+reset_graph()
+a =tf.Variable(0.2,name='a')
+b = tf.Variable(0.3,name='b')
+z= tf.constant(0.0,name='z0')
+
+for i in range(100):
+    z = a * tf.cos(z + i) + z * tf.sin(b - i)
+
+grads = tf.gradients(z,[a,b])
+init = tf.global_variables_initializer()
+
+with tf.Session() as sess:
+    init.run()
+    print(z.eval())
+    print(sess.run(grads))
+
+# Using an GradientDescentOptimizer
+
+
+reset_graph()
+
+n_epochs = 1000
+learning_rate = 0.01
+
+X = tf.constant(scaled_housing_data_plus_bias, dtype=tf.float32, name="X")
+y = tf.constant(housing.target.reshape(-1, 1), dtype=tf.float32, name="y")
+theta = tf.Variable(tf.random_uniform([n + 1, 1], -1.0, 1.0, seed=42), name="theta")
+y_pred = tf.matmul(X, theta, name="predictions")
+error = y_pred - y
+mse = tf.reduce_mean(tf.square(error), name="mse")
+
+optimizer =tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
+training_op = optimizer.minimize(mse)
+
+init = tf.global_variables_initializer()
+
+with tf.Session() as sess:
+    sess.run(init)
+
+    for epoch in range(n_epochs):
+        if epoch % 100 == 0:
+            print("Epoch", epoch, "MSE =", mse.eval())
+        sess.run(training_op)
+
+    best_theta = theta.eval()
+
+print("Best theta:")
+print(best_theta)
+
+
+# Using an MomentumOptimizer
+
+
+reset_graph()
+
+n_epochs = 1000
+learning_rate = 0.01
+
+X = tf.constant(scaled_housing_data_plus_bias, dtype=tf.float32, name="X")
+y = tf.constant(housing.target.reshape(-1, 1), dtype=tf.float32, name="y")
+theta = tf.Variable(tf.random_uniform([n + 1, 1], -1.0, 1.0, seed=42), name="theta")
+y_pred = tf.matmul(X, theta, name="predictions")
+error = y_pred - y
+mse = tf.reduce_mean(tf.square(error), name="mse")
+
+optimizer = tf.train.MomentumOptimizer(learning_rate=learning_rate,
+                                       momentum=0.9)
+training_op = optimizer.minimize(mse)
+
+init = tf.global_variables_initializer()
+
+with tf.Session() as sess:
+    sess.run(init)
+
+    for epoch in range(n_epochs):
+        if epoch % 100 == 0:
+            print("Epoch", epoch, "MSE =", mse.eval())
+        sess.run(training_op)
+
+    best_theta = theta.eval()
+
+print("Best theta:")
+print(best_theta)
