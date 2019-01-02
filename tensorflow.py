5dba11706cf4a836ffdf523cdb37b44b452adcab
hrajgarhia <hrajgarhia943@gmail.com>
2019-01-02 19:27:46 +0530

    Solving linear regression using Mini-Batch Gradient Descent

--- tensorflow.py
+++ tensorflow.py
@@ -273,3 +273,57 @@ with tf.Session() as sess:
 
 print("Best theta:")
 print(best_theta)
+
+# Placeholders
+
+# Placeholders are special because they don't actually perform any computation,
+# they just ouput the data we tell them to output at runtime
+
+
+# Placeholder practice
+
+A = tf.placeholder(tf.float32, shape =(None,3))
+B = A + 5
+with tf.Session() as sess:
+    B_val_1 = B.eval(feed_dict={A:[[1,2,3]]})
+    B_val_2 = B.eval(feed_dict={A:[[4,5,6],[7,8,9]]})
+print(B_val_1)
+print(B_val_2)
+
+# Mini - Batch Gradient Descent
+
+n_epochs = 10
+learning_rate = 0.01
+reset_graph()
+
+X = tf.placeholder(tf.float32, shape=(None, n + 1), name="X")
+y = tf.placeholder(tf.float32, shape=(None, 1), name="y")
+
+theta = tf.Variable(tf.random_uniform([n + 1, 1], -1.0, 1.0, seed=42), name="theta")
+y_pred = tf.matmul(X, theta, name="predictions")
+error = y_pred - y
+mse = tf.reduce_mean(tf.square(error), name="mse")
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
+training_op = optimizer.minimize(mse)
+
+init = tf.global_variables_initializer()
+
+batch_size = 100
+n_batches = int(np.ceil(m / batch_size))
+
+
+def fetch_batch(epoch,batch_index,batch_size):
+    np.random.seed(epoch*n_batches+batch_index)
+    indices = np.random.randint(m,size=batch_size)
+    X_batch = scaled_housing_data_plus_bias[indices]
+    y_batch = housing.target.reshape(-1,1)[indices]
+    return X_batch, y_batch
+
+with tf.Session() as sess:
+    sess.run(init)
+
+    for epoch in range(n_epochs):
+        for batch_index in range(n_batches):
+            X_batch, y_batch = fetch_batch(epoch,batch_index,batch_size)
+            sess.run(training_op, feed_dict = {X: X_batch,y:y_batch})
+    best_theta = theta.eval()
