61f1735da0d53f0032c813ade4fc6a09144a797c
hrajgarhia <hrajgarhia943@gmail.com>
2019-01-22 14:15:21 +0530

    Commits:-
    1. Batch normalisation,
    2. Batch normalisation using partial function,
    3. Gradient Clipping,
    4. Reusing pre-trained layers.

--- DNNs.py
+++ DNNs.py
@@ -155,3 +155,277 @@ with tf.Session() as sess:
             print(epoch, "Batch accuracy:", acc_batch, "Validation accuracy:", acc_valid)
 
     save_path = saver.save(sess, "./my_model_final_selu.ckpt")
+
+
+# Batch normalisation
+
+reset_graph()
+import tensorflow as tf
+
+n_inputs = 28 * 28
+n_hidden1 = 300
+n_hidden2 = 100
+n_outputs = 10
+
+X= tf.placeholder(tf.float32,shape=(None,n_inputs), name = 'X')
+training  = tf.placeholder_with_default(False,shape = (), name = 'training')
+hidden1 = tf.layer.dense(X,n_hidden1,name = 'hidden1')
+bn1 = tf.layers.batch_normalization(hidden1,training=training,momentum = 0.9)
+bn1_act =tf.nn.elu(bn1)
+
+hidden2 = tf.layer.dense(bn1_act,n_hidden2,name = 'hidden2')
+bn2 = tf.layers.batch_normalization(hidden2,training=training,momentum = 0.9)
+bn2_act =tf.nn.elu(bn2)
+
+logits_before_bn = tf.layers.dense(bn2_act,n_outputs,name = 'outputs')
+logits = tf.layers.batch_normalization(logits_before_bn,training=training,momentum = 0.9)
+
+# Batch normalisation using partial
+
+reset_graph()
+
+X = tf.placeholder(tf.float32, shape=(None, n_inputs), name="X")
+training = tf.placeholder_with_default(False, shape=(), name='training')
+
+from functools import partial
+
+my_batch_norm_layer = partial(tf.layers.batch_normalization,
+                              training=training, momentum=0.9)
+
+hidden1 = tf.layers.dense(X, n_hidden1, name="hidden1")
+bn1 = my_batch_norm_layer(hidden1)
+bn1_act = tf.nn.elu(bn1)
+hidden2 = tf.layers.dense(bn1_act, n_hidden2, name="hidden2")
+bn2 = my_batch_norm_layer(hidden2)
+bn2_act = tf.nn.elu(bn2)
+logits_before_bn = tf.layers.dense(bn2_act, n_outputs, name="outputs")
+logits = my_batch_norm_layer(logits_before_bn)
+
+# Batch normalisation using partial fn and He initialiser
+
+reset_graph()
+
+batch_norm_momentum = 0.9
+learning_rate = 0.01
+
+X = tf.placeholder(tf.float32, shape=(None, n_inputs), name="X")
+y = tf.placeholder(tf.int32, shape=(None), name="y")
+training = tf.placeholder_with_default(False, shape=(), name='training')
+
+with tf.name_scope("dnn"):
+    he_init = tf.variance_scaling_initializer()
+
+    my_batch_norm_layer = partial(
+        tf.layers.batch_normalization,
+        training=training,
+        momentum=batch_norm_momentum)
+
+    my_dense_layer = partial(
+        tf.layers.dense,
+        kernel_initializer=he_init)
+
+    hidden1 = my_dense_layer(X, n_hidden1, name="hidden1")
+    bn1 = tf.nn.elu(my_batch_norm_layer(hidden1))
+    hidden2 = my_dense_layer(bn1, n_hidden2, name="hidden2")
+    bn2 = tf.nn.elu(my_batch_norm_layer(hidden2))
+    logits_before_bn = my_dense_layer(bn2, n_outputs, name="outputs")
+    logits = my_batch_norm_layer(logits_before_bn)
+
+with tf.name_scope("loss"):
+    xentropy = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y, logits=logits)
+    loss = tf.reduce_mean(xentropy, name="loss")
+
+with tf.name_scope("train"):
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate)
+    training_op = optimizer.minimize(loss)
+
+with tf.name_scope("eval"):
+    correct = tf.nn.in_top_k(logits, y, 1)
+    accuracy = tf.reduce_mean(tf.cast(correct, tf.float32))
+
+init = tf.global_variables_initializer()
+saver = tf.train.Saver()
+
+n_epochs = 20
+batch_size = 200
+
+extra_update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+
+with tf.Session() as sess:
+    init.run()
+    for epoch in range(n_epochs):
+        for X_batch, y_batch in shuffle_batch(X_train, y_train, batch_size):
+            sess.run([training_op, extra_update_ops],
+                     feed_dict={training: True, X: X_batch, y: y_batch})
+        accuracy_val = accuracy.eval(feed_dict={X: X_valid, y: y_valid})
+        print(epoch, "Validation accuracy:", accuracy_val)
+
+    save_path = saver.save(sess, "./my_model_final_bn.ckpt")
+
+
+# Note that you could also make the training operation depend on the update operations:
+
+with tf.name_scope("train"):
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate)
+    extra_update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+    with tf.control_dependencies(extra_update_ops):
+        training_op = optimizer.minimize(loss)
+
+#This way, you would just have to evaluate the training_op during training,
+# TensorFlow would automatically run the update operations as well:
+
+sess.run(training_op, feed_dict={training: True, X: X_batch, y: y_batch})
+
+[v.name for v in tf.trainable_variables()]
+[v.name for v in tf.global_variables()]
+
+# Gradient clipping
+reset_graph()
+
+n_inputs = 28 * 28  # MNIST
+n_hidden1 = 300
+n_hidden2 = 50
+n_hidden3 = 50
+n_hidden4 = 50
+n_hidden5 = 50
+n_outputs = 10
+
+X = tf.placeholder(tf.float32, shape=(None, n_inputs), name="X")
+y = tf.placeholder(tf.int32, shape=(None), name="y")
+
+with tf.name_scope("dnn"):
+    hidden1 = tf.layers.dense(X, n_hidden1, activation=tf.nn.relu, name="hidden1")
+    hidden2 = tf.layers.dense(hidden1, n_hidden2, activation=tf.nn.relu, name="hidden2")
+    hidden3 = tf.layers.dense(hidden2, n_hidden3, activation=tf.nn.relu, name="hidden3")
+    hidden4 = tf.layers.dense(hidden3, n_hidden4, activation=tf.nn.relu, name="hidden4")
+    hidden5 = tf.layers.dense(hidden4, n_hidden5, activation=tf.nn.relu, name="hidden5")
+    logits = tf.layers.dense(hidden5, n_outputs, name="outputs")
+
+with tf.name_scope("loss"):
+    xentropy = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y, logits=logits)
+    loss = tf.reduce_mean(xentropy, name="loss")
+
+learning_rate = 0.01
+threshold = 1.0
+
+optimizer = tf.train.GradientDescentOptimizer(learning_rate)
+grads_and_vars = optimizer.compute_gradients(loss)
+capped_gvs = [(tf.clip_by_value(grad, -threshold, threshold), var)
+              for grad, var in grads_and_vars]
+training_op = optimizer.apply_gradients(capped_gvs)
+
+with tf.name_scope("eval"):
+    correct = tf.nn.in_top_k(logits, y, 1)
+    accuracy = tf.reduce_mean(tf.cast(correct, tf.float32), name="accuracy")
+
+init = tf.global_variables_initializer()
+saver = tf.train.Saver()
+
+n_epochs = 20
+batch_size = 200
+
+with tf.Session() as sess:
+    init.run()
+    for epoch in range(n_epochs):
+        for X_batch, y_batch in shuffle_batch(X_train, y_train, batch_size):
+            sess.run(training_op, feed_dict={X: X_batch, y: y_batch})
+        accuracy_val = accuracy.eval(feed_dict={X: X_valid, y: y_valid})
+        print(epoch, "Validation accuracy:", accuracy_val)
+
+    save_path = saver.save(sess,"C://Users//USER//PycharmProjects//Neural-networks-and-deep-learning//graphs//my_model_final.ckpt")
+
+
+# Reusing pre-trained layers
+
+reset_graph()
+n_hidden4 = 20
+n_outputs = 10
+
+saver = tf.train.import_meta_graph("C://Users//USER//PycharmProjects//Neural-networks-and-deep-learning//graphs//my_model_final.ckpt")
+
+X = tf.get_default_graph().get_tensor_by_name('X:0')
+y = tf.get_default_graph().get_tensor_by_name('y:0')
+
+hidden3 = tf.get_default_graph().get_tensor_by_name('dnn/hidden3/Relu:0')
+
+new_hidden4 = tf.layers.dense(hidden3,n_hidden4,activation = tf.nn.relu,name = 'new_hidden4')
+new_logits = tf.layers.dense(new_hidden4,n_outputs,name='new_outputs')
+
+with tf.name_scope("new_loss"):
+    xentropy = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y, logits=new_logits)
+    loss = tf.reduce_mean(xentropy, name="loss")
+
+with tf.name_scope("new_eval"):
+    correct = tf.nn.in_top_k(new_logits, y, 1)
+    accuracy = tf.reduce_mean(tf.cast(correct, tf.float32), name="accuracy")
+
+with tf.name_scope("new_train"):
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate)
+    training_op = optimizer.minimize(loss)
+
+init = tf.global_variables_initializer()
+new_saver = tf.train.Saver()
+with tf.Session() as sess:
+    init.run()
+    saver.restore(sess, "./my_model_final.ckpt")
+
+    for epoch in range(n_epochs):
+        for X_batch, y_batch in shuffle_batch(X_train, y_train, batch_size):
+            sess.run(training_op, feed_dict={X: X_batch, y: y_batch})
+        accuracy_val = accuracy.eval(feed_dict={X: X_valid, y: y_valid})
+        print(epoch, "Validation accuracy:", accuracy_val)
+
+    save_path = new_saver.save(sess, "./my_new_model_final.ckpt")
+
+# If you have access to the Python code that built the original graph, you can just reuse the
+# parts you need and drop the rest:
+
+reset_graph()
+n_inputs = 28*28
+n_hidden1 = 300
+n_hidden2 = 50
+n_hidden3 = 50
+n_hidden4 = 20
+n_outputs = 10
+
+
+X = tf.placeholder(tf.float32, shape=(None, n_inputs), name="X")
+y = tf.placeholder(tf.int32, shape=(None), name="y")
+
+with tf.name_scope("dnn"):
+    hidden1 = tf.layers.dense(X, n_hidden1, activation=tf.nn.relu, name="hidden1")       # reused
+    hidden2 = tf.layers.dense(hidden1, n_hidden2, activation=tf.nn.relu, name="hidden2") # reused
+    hidden3 = tf.layers.dense(hidden2, n_hidden3, activation=tf.nn.relu, name="hidden3") # reused
+    hidden4 = tf.layers.dense(hidden3, n_hidden4, activation=tf.nn.relu, name="hidden4") # new!
+    logits = tf.layers.dense(hidden4, n_outputs, name="outputs")                         # new!
+
+with tf.name_scope("loss"):
+    xentropy = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y, logits=logits)
+    loss = tf.reduce_mean(xentropy, name="loss")
+
+with tf.name_scope("eval"):
+    correct = tf.nn.in_top_k(logits, y, 1)
+    accuracy = tf.reduce_mean(tf.cast(correct, tf.float32), name="accuracy")
+
+with tf.name_scope("train"):
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate)
+    training_op = optimizer.minimize(loss)
+
+reuse_vars = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES,scope = "hidden[123]")
+restore_saver = tf.train.Saver(reuse_vars)
+init = tf.global_variables_initializer()
+saver = tf.train.Saver()
+
+with tf.Session() as sess:
+    init.run()
+    restore_saver.restore(sess, "./my_model_final.ckpt")
+
+    for epoch in range(n_epochs):                                            # not shown in the book
+        for X_batch, y_batch in shuffle_batch(X_train, y_train, batch_size): # not shown
+            sess.run(training_op, feed_dict={X: X_batch, y: y_batch})        # not shown
+        accuracy_val = accuracy.eval(feed_dict={X: X_valid, y: y_valid})     # not shown
+        print(epoch, "Validation accuracy:", accuracy_val)                   # not shown
+
+    save_path = saver.save(sess, "./my_new_model_final.ckpt")
+
+# reusing models from other frameworks
